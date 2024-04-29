97be05eda8d661486ed3344cf8efbec0db2916ea
TentCityTech <joshh@tentcitytech.com>
2024-04-29 14:26:41 -0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="flex flex-col justify-center items-center mx-auto max-w-7xl sm:px-6 lg:px-8 h-dvh">
+	<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,22 @@
+<img src="/deadandtattooed.png" width="50%" alt="logo" />
+<div class="my-4 w-1/2">
+	<a
+		class=" transition ease-in-out btn bg-black hover:bg-gray-800 hover:drop-shadow-lg hover:scale-105 text-white w-full"
+		target="_blank"
+		href="https://www.etsy.com/shop/DeadandTattooed"
+	>
+		Shop on Etsy</a
+	>
+</div>
+
+<h2 class="text-xl font-bold">Just a guy who stabs dead things</h2>
+<article class="prose text-center">
+	<p>Come check out my store.</p>
+	<!-- ... -->
+</article>
+
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
+</style>
